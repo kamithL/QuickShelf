069301f18a73afe45561d466ea4ffd1880ddb6fc
KamithL <kamithlakmal@gmail.com>
2025-06-12 15:23:43 +0530

    feat: Polish Home and AddItem UI with production-level consistency

---
+++ app/item-detail.tsx
@@ -0,0 +1,7 @@
+// app/item-detail.tsx
+import React from 'react';
+import ItemDetailScreen from '../src/screens/ItemDetailScreen';
+
+export default function ItemDetailPage() {
+  return <ItemDetailScreen />;
+}
