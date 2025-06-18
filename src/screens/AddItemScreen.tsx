a9ad9130ef92ebf024b76bf927ed953800f1a4d2
Kamith <kamithlakmal@gmail.com>
2025-06-18 12:35:40 +0530

    Fix dropdown reset for category

--- src/screens/AddItemScreen.tsx
+++ src/screens/AddItemScreen.tsx
@@ -87,7 +87,7 @@ export default function AddItemScreen() {
     setImage(null); 
     setShowErrors(false);
     setShowLocationDropdown(false);
-    setShowLocationDropdown(false)
+    setShowCategoryDropdown(false);
     router.replace('/');
   };
 
